77b5122b301ba1116c29e7a38e5d2c105f67ccb4
harryborison920613 <harryborison920613@gmail.com>
2023-12-28 13:45:46 -0600

    Change sdkclaro to not ssr

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,10 @@
 import '@/styles/globals.css'
-import * as sdkclaro from '@claro/sdkclaro'
+import dynamic from 'next/dynamic';
+
+const sdkclaro = dynamic(
+  () => import('@claro/sdkclaro'),
+  { ssr: false }
+);
 
 export default function App ({ Component, pageProps }) {
   return <Component {...pageProps} />
