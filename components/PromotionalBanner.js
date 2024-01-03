778de12fcbffa0fcb63b26ac97cfbf48c542f177
harryborison920613 <harryborison920613@gmail.com>
2024-01-03 00:13:49 -0400

    f

--- components/PromotionalBanner.js
+++ components/PromotionalBanner.js
@@ -2,8 +2,8 @@ import Link from 'next/link';
 import Image from 'next/image';
 
 const PromotionalBanner = ({ banner }) => {
-  const bannerLink = banner[0].link;
-  const bannerImage = banner[0].img
+  const bannerLink = banner[0]?.link ? banner[0]?.link : '';
+  const bannerImage = banner[0]?.img ? banner[0]?.img : ''
 
   return (
     <Link href={bannerLink} passHref>
